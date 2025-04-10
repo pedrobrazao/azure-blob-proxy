4fcddc9e6b7cf7dceda0148b865a1cbb596be627
Pedro Ferreira <pedro@thingston.com>
2025-04-10 16:29:05 +0100

    Updated get container handler

--- src/Handler/GetContainerHandler.php
+++ src/Handler/GetContainerHandler.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\Handler;
 
 use App\Exception\InvalidContainerException;
+use App\Exception\InvalidOperationException;
 use App\Validator\ContainerNameValidator;
 use AzureOss\Storage\Blob\BlobServiceClient;
-use AzureOss\Storage\Blob\Models\Blob;
-use AzureOss\Storage\Blob\Models\GetBlobsOptions;
+use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -21,36 +21,47 @@ final class GetContainerHandler
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $query = $request->getQueryParams();
-        $body = $response->getBody();
-
-        $name = $args['container'] ?? '';
-        $validator = new ContainerNameValidator($name);
+        $validator = new ContainerNameValidator($args['container']);
 
         if (false === $validator->isValid()) {
-            throw new InvalidContainerException;
+            throw new InvalidContainerException($validator->getError());
         }
 
-        $prefix = $query['prefix'] ?? null;
-        $options = isset($query['pagesize']) ? new GetBlobsOptions((int) $query['pagesize']) : null;
-        $json = json_encode($this->listBlobs($name, $prefix, $options));
-
-        $body->write($json);
-
-        return $response->withHeader('content-type', 'application/json')->withBody($body);
+switch ($request->getQueryParams()['op'] ?? null) {
+    case 'list':
+        return $this->listBlobs($request, $response, $args);
+        case 'props':
+            return $this->getProperties($response, $args);
+}
+        
+throw new InvalidOperationException();
     }
 
-    /**
-     * @return Blob[]
-     */
-    private function listBlobs(string $name, string $prefix = null, ?GetBlobOptions $options = null): array
+    private function listBlobs(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $blobs = [];
-        $client = $this->blobServiceClient->getContainerClient($name);
+        $client = $this->blobServiceClient->getContainerClient($args['container']);
+        $prefix = $request->getQueryParams()['prefix'] ?? '';
 
-        foreach ($client->getBlobs($prefix, $options) as $blob) {
+        foreach ($client->getBlobs($prefix) as $blob) {
             $blobs[] = $blob;
         }
-        return $blobs;
+
+        $json = json_encode($blobs);
+        $body = $response->getBody();
+        $body->write($json);
+
+        return $response->withStatus(StatusCodeInterface::STATUS_OK)->withHeader('content-type', 'application/json')->withBody($body);
+    }
+
+    private function getProperties(ResponseInterface $response, array $args): ResponseInterface
+    {
+        $properties = $this->blobServiceClient->getContainerClient($args['container'])->getProperties();
+        $json = json_encode($properties);
+
+        $body = $response->getBody();
+        $body->write($json);
+
+        return $response->withStatus(StatusCodeInterface::STATUS_OK)->withHeader('content-type', 'application/json')->withBody($body);
     }
 }
